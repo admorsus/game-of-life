706c8f3eda318b835332cddd886a192b65bc68ab
mb <m.bautista@alumnos.upm.es>
2017-12-05 20:35:04 +0100

    Working on...

--- game-of-life.c
+++ game-of-life.c
@@ -1,14 +1,23 @@
+// Made by Miguel Bautista Pérez
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#define byte char
+
+typedef char byte;
+
+typedef struct MAT
+{
+    unsigned int h; //heigth
+    unsigned int l; //length
+    unsigned int ** rack; //rack
+} MAT;
 
 int aint		(byte, byte, char**, int);
 char achar		(byte, byte, char**, char);
 int ** maker	(int, int);
 int ** fillr	(int, int, int**, byte);
-int ** cornway	(int, int, int**);
+int ** conway	(int, int, int**);
 int ** getcells	(int, int, int**);
 void printr		(int, int, int**, char, char, char, char, char);
 byte locals		(int, int, int**, int, int);
@@ -17,6 +26,8 @@ static byte activity=1;
 
 int main (byte argc, char ** args)
 {
+	MAT mat;
+	
 	int l=20, h=20;
 	int t=200;
 	int i=-1;
@@ -62,7 +73,7 @@ int main (byte argc, char ** args)
 		activity=0;
 		printf("\e[1;1H\e[2J");
 		printr(l,h,rack,ca,cd,ch,cv,cc);
-		rack=cornway(l, h, rack);
+		rack=conway(l, h, rack);
 		iter++;
 		usleep(t*1000);
 	}
@@ -105,26 +116,35 @@ char achar (byte a, byte argc, char ** args, char def)
 	else return def;
 }
 
-int ** cornway (int l, int h, int ** rack)
+void conway (MAT *mat)
 {
-	int ** newr=maker(l, h);
+	MAT gen;
+	int l= mat->l;
+	int h= mat->h;
+	make_r(&gen, l, h);
 	for (int y=0; y<h; y++)
 	{
 		for (int x=0; x<l; x++)
 		{
-			byte n=locals(l,h,rack,x,y);
-			if (rack[x][y] && n==2) newr[x][y]=1;
-			else if (n==3) newr[x][y]=1;
-			else newr[x][y]=0;
-			if (rack[x][y]!=newr[x][y]) activity=1;
+			char n= locals(mat->rack, x, y);
+			if (mat->rack[x][y] && n==2)
+			    gen->rack[x][y]= 1;
+			else if (n==3)
+			    gen->rack[x][y]= 1;
+			else
+			    gen->rack[x][y]= 0;
+			if (mat->rack[x][y] != gen->rack[x][y])
+			    activity=1;
 		}
 	}
-	return newr;
+	return activity;
 }
 
-int ** getcells (int l, int h, int ** rack)
+void getcells (MAT *mat)
 {
 	int c=0, nx, ny;
+	int l= mat->l;
+	int h= mat->h;
 	printf("\e[1;1H\e[2J");
 	printf("Input cells mode, input X and Y for each cell:\n");
 	printf(" at any time: -1: Show status, -2: Stop input.\n\n");
@@ -134,7 +154,7 @@ int ** getcells (int l, int h, int ** rack)
 		scanf("%d", &nx);
 		if (nx==-1)
 		{
-			printr(l,h,rack,'@','.',0,0,0);
+			printr(mat,'@','.',0,0,0);
 			continue;
 		}
 		else if (nx<=-2) break;
@@ -142,80 +162,102 @@ int ** getcells (int l, int h, int ** rack)
 		scanf("%d", &ny);
 		if (ny==-1)
 		{
-			printr(l,h,rack,'@','.',0,0,0);
+			printr(mat,'@','.',0,0,0);
 			continue;
 		}
 		else if (ny<=-2) break;
-		if (l>nx && h>ny) rack[nx][ny]=1;
+		if (nx<l && ny<h) mat->rack[nx][ny]=1;
 		else printf("Grid size: %dx%d, change it with -s option\n",l,h);
 	}
-	return rack;
 }
 
-byte locals (int l, int h, int ** rack, int x, int y)
+char locals (MAT *mat, int x, int y)
 {
-	byte ul,um,ur,ml,mr,bl,bm,br;
-	l--; h--;
-	if (y<=0 || x<=0)	ul=0; else ul=rack[x-1][y-1];
-	if (y<=0)			um=0; else um=rack[x-0][y-1];
-	if (y<=0 || x>=l)	ur=0; else ur=rack[x+1][y-1];
-	if		   (x<=0)	ml=0; else ml=rack[x-1][y-0];
-	if		   (x>=l)	mr=0; else mr=rack[x+1][y-0];
-	if (y>=h || x<=0)	bl=0; else bl=rack[x-1][y+1];
-	if (y>=h)			bm=0; else bm=rack[x-0][y+1];
-	if (y>=h || x>=l)	br=0; else br=rack[x+1][y+1];
+	char ul,um,ur,ml,mr,bl,bm,br;
+	int l= mat->l -1;
+	int h= mat->h -1;
+	if (y<=0 || x<=0) ul=0;
+	else ul= mat->rack[x-1][y-1];
+	if (y<=0) um=0;
+	else um= mat->rack[x-0][y-1];
+	if (y<=0 || x>=l) ur=0;
+	else ur= mat->rack[x+1][y-1];
+	if (x<=0) ml=0;
+	else ml= mat->rack[x-1][y-0];
+	if (x>=l) mr=0;
+	else mr= mat->rack[x+1][y-0];
+	if (y>=h || x<=0) bl=0;
+	else bl= mat->rack[x-1][y+1];
+	if (y>=h) bm=0;
+	else bm= mat->rack[x-0][y+1];
+	if (y>=h || x>=l) br=0;
+	else br= mat->rack[x+1][y+1];
 	return ul+um+ur+ml+mr+bl+bm+br;
 }
 
-int ** maker (int l, int h)
+void make_r (MAT *mat, unsigned int l, unsigned int h)
 {
-	int ** rack=malloc(l*sizeof(int**));
-	if (rack==NULL) exit(1);
+	mat->rack= malloc(l * sizeof(int**));
+	if (mat->rack == NULL)
+	    exit(1);
 	for (int x=0; x<l; x++)
 	{
-		rack[x]=malloc(h*sizeof(int*));
-		if (rack[x]==NULL) exit(1);
+		mat->rack[x]= malloc(h * sizeof(int*));
+		if (mat->rack[x] == NULL)
+		    exit(1);
 		for (int y=0; y<h; y++)
-		{
-			rack[x][y]=0;
-		}
+		    mat->rack[x][y]= 0;
 	}
-	return rack;
 }
 
-int ** fillr (int l, int h, int ** rack, byte prob)
+void fill_r (MAT *mat)
 {
-	int seed=time(NULL);
+	int l= mat->l;
+	int h= mat->h;
+	srand(time(NULL));
 	for (int x=0; x<l; x++)
 	{
 		for (int y=0; y<h; y++)
 		{
-			srand(seed+(x*x*y+y+x/2));
-			byte random=rand()%100;
-			if (random<prob) rack[x][y]=1;
+			char random= rand()%100;
+			if (random<prob)
+			    mat->rack[x][y]=1;
 		}
 	}
-	return rack;
 }
 
-void printr (int l, int h, int ** rack, char a, char d, char delim_z, char delim_v, char delim_c)
+void print_r (MAT *mat, char a, char d, char delim_z, char delim_v, char delim_c)
 {
-	printf("%c",delim_c);
-	if (delim_z) for (int i=0; i<=l*2; i++) printf("%c",delim_z);
-	if (delim_c) printf("%c\n",delim_c);
+    int l= mat->l;
+	int h= mat->h;
+    if (delim_c)
+	    printf("%c",delim_c);
+	if (delim_z)
+	    for (int i=0; i<=l*2; i++)
+	        printf("%c",delim_z);
+	if (delim_c)
+	    printf("%c\n",delim_c);
 	for (int y=0; y<h; y++)
 	{
-		if (delim_v) printf("%c ",delim_v);
+		if (delim_v)
+		    printf("%c ",delim_v);
 		for (int x=0; x<l; x++)
 		{
-			if (rack[x][y]) printf("%c",a);
-			else printf("%c",d);
+			if (mat->rack[x][y])
+			    printf("%c",a);
+			else
+			    printf("%c",d);
 			printf(" ");
 		}
-		if (delim_v) printf("%c",delim_v);
+		if (delim_v)
+		    printf("%c",delim_v);
 		printf("\n");
 	}
-	if (delim_c) printf("%c",delim_c);
-	if (delim_z) for (int i=0; i<=l*2; i++) printf("%c",delim_z);
-	if (delim_c) printf("%c\n",delim_c);
+	if (delim_c)
+	    printf("%c",delim_c);
+	if (delim_z)
+	    for (int i=0; i<=l*2; i++)
+	        printf("%c",delim_z);
+	if (delim_c)
+	    printf("%c\n",delim_c);
 }
