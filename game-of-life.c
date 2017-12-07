e340d0d5640c38ee8aae42f1935770770803a5f9
mb <m.bautista@alumnos.upm.es>
2017-12-07 17:52:35 +0100

    New version of the program! (Now it uses structs and good arg parsing)

--- game-of-life.c
+++ game-of-life.c
@@ -6,132 +6,151 @@
 
 typedef struct MAT
 {
-    unsigned h; //heigth
-    unsigned l; //length
+    int h; //heigth
+    int l; //length
     char **rack;    //rack
 } MAT;
 
-void help (char*);
-unsigned aint (char, char, char**, unsigned);
-char achar (char, char, char**, char);
+void show_help (char*);
 char conway (MAT*);
-void get_cells (MAT*);
-char locals (MAT*, unsigned, unsigned);
-void make_r (MAT*, unsigned, unsigned);
+void input_mode (MAT*);
+char locals (MAT*, int, int);
+void make_r (MAT*, int, int);
 void fill_r (MAT*, char);
 void print_r (MAT*, char, char, char, char, char);
 
-unsigned main (char argc, char **args)
-{
-	char reset[20];
+int main (char argc, char **argv)
+{    
+    const char *txtHelp= "[OPTION] [PARAMETER] [...]\n"
+        "A customisable Conway's Game of Life for shell.\n"
+        "\n"
+        "Options:\n"
+        " -x N       Set grid length to N\n"
+        " -y N       Set grid heigth to N\n"
+        " -t N       Set time between iterations to N\n"
+        " -i N       Set maximum iterations to N\n"
+        " -r N       Set appearance probability to N\n"
+        " -p         Start input cells mode\n"
+        " -l         Show all delimiters\n"
+        " -a C       Represent alive cells with C\n"
+        " -d C       Represent dead cells with C\n"
+        " -z C       Represent horizontal delimiters with C\n"
+        " -v C       Represent vertical delimiters with C\n"
+        " -c C       Represent corner delimiters with C\n"
+        " -q         Do not show final message\n"
+        " -h         Show this message and exits\n";
+	char txtReset[20];
     FILE *fp = popen ("tput reset", "r");
-    int resetsz= fread (reset, 1, sizeof(reset), fp);
+    int szReset= fread (txtReset, 1, sizeof(txtReset), fp);
     pclose (fp);
 	
 	MAT mat;
+	char verbosity= 1;
+	int length= 20;
+	int heigth= 20;
+	int time= 200;
+    int iter= 0;
+    int maxIter= 0; //0 equals infinite
+    char appProb=33;
+	char inputMode= 0;
+	char showAllDel= 0;
+	char cAlive='@', cDead=' ';
+	char delHoriz=0, delVert=0, delCorner=0;
 	
-	unsigned l=20, h=20;
-	unsigned t=200;
-	int i=-1;
-	char r=33;
-	char p=0;
-	char ca='@', cd=' ', ch=0, cv=0, cc=0;
-	
-	for (char a=1; a<argc; a++)
+	const char *opts= "hqplx:y:t:i:r:a:d:z:v:c:";
+	int opt;
+	while ((opt= getopt(argc, argv, opts)) != -1)
 	{
-		if (args[a][0]=='-')
-		{
-			switch (args[a][1])
-			{
-				case 's': l=aint(a,argc,args,l); h=aint(a+1,argc,args,h); break;
-				case 't': t=aint(a,argc,args,t); break;
-				case 'r': r=aint(a,argc,args,r); break;
-				case 'p': p=1; break;
-				case 'i': i=aint(a,argc,args,i); break;
-				case 'c': switch (args[a][2])
-				{
-					case 'a': ca=achar(a,argc,args,ca); break;
-					case 'd': cd=achar(a,argc,args,cd); break;
-					case 'h': ch=achar(a,argc,args,ch); break;
-					case 'v': cv=achar(a,argc,args,cv); break;
-					case 'c': cc=achar(a,argc,args,cc); break;
-					default: ch='~'; cv='|'; cc='+';
-				}
-				break;
-				case 'h': help(args[0]); return 0;
-				default:
-					printf("%s: incorrect option -- «%c»\n",args[0] , args[a][1]);
-					printf("Use '%s -h' for more info.\n", args[0]);
-					return 1;
-			}
-		}
+        switch (opt) {
+            case 'h':
+                printf("Usage: %s %s\n", argv[0], txtHelp);
+                return 0;
+                break;
+            case 'x':
+                length= atoi(optarg);
+                break;
+            case 'y':
+                heigth= atoi(optarg);
+                break;
+            case 't':
+                time= atoi(optarg);
+                break;
+            case 'i':
+                maxIter= atoi(optarg);
+                break;
+            case 'r':
+                appProb= atoi(optarg);
+                break;
+            case 'p':
+                inputMode= 1;
+                break;
+            case 'l':
+                showAllDel= 1;
+                break;
+            case 'a':
+                cAlive= optarg[0];
+                break;
+            case 'd':
+                cDead= optarg[0];
+                break;
+            case 'z':
+                delHoriz= optarg[0];
+                break;
+            case 'v':
+                delVert= optarg[0];
+                break;
+            case 'c':
+                delCorner= optarg[0];
+                break;
+            case 'q':
+                verbosity= 0;
+                break;
+            default:
+                printf("Try '%s -h' for more information.\n", argv[0]);
+                return 1;
+        }
+    }
+	
+	make_r(&mat, length, heigth);
+	if (inputMode)
+	{
+	    write(1, txtReset, szReset);
+	    input_mode(&mat);
 	}
-	unsigned iter=0;
-	
-	make_r(&mat, l, h);
-	
-	if (p)
-	    get_cells(&mat);
 	else
-	    fill_r(&mat, r);
+	    fill_r(&mat, appProb);
 	    
-	do {
-	    write(1, reset, resetsz);
-		print_r(&mat, ca,cd,ch,cv,cc);
-		usleep(t*1000);
-		iter++;
+	if (showAllDel)
+	{
+	    if (!delHoriz) delHoriz= '~';
+	    if (!delVert) delVert= '|';
+	    if (!delCorner) delCorner= '+';
 	}
-	while (conway(&mat) && iter<i);
+	    
+	do //main loop
+	{
+	    write(1, txtReset, szReset);
+		print_r(&mat, cAlive, cDead, delHoriz, delVert, delCorner);
+		usleep(time *1000);
+		iter++;
+	} while (conway(&mat) && iter!=maxIter);
 	
-    if (i)
+    if (verbosity)
         printf("%d iterations realised.\n", iter);
     free(mat.rack);
     return 0;
 }
 
-void help (char * name)
-{
-	printf("Usage: %s [OPTION] [PARAMETER]...\n", name);
-	printf("A customisable Conway's Game of Life for shell.\n");
-	printf(" see <https://en.wikipedia.org/wiki/Conway's_Game_of_Life>\n\n");
-	printf("Options:\n");
-	printf(" -s [X][Y]	Set grid legth to X and heigth to Y\n");
-	printf(" -t [N]		Set time between iterations to N miliseconds\n");
-	printf(" -i [N]		N is the maximum number of iterations (0 to not show)\n");
-	printf(" -r [N]		Spread random cells with N probability\n");
-	printf(" -p		Start input cells mode, instead of spread them randomly\n");
-	printf(" -c		Print delimiters\n");
-	printf(" -ca [C]	Represent living cells with C\n");
-	printf(" -cd [C]	Represent dead cells with C\n");
-	printf(" -ch [C]	Print horizontal delimiter, representd with C\n");
-	printf(" -cv [C]	Print vertical delimiter, representd with C\n");
-	printf(" -cc [C]	Print corner delimiter, representd with C\n");
-}
-
-unsigned aint (char a, char argc, char ** args, unsigned p)
-{
-	a++;
-	if (a<argc && args[a][0]!='-') return atoi(args[a]);
-	else return p;
-}
-
-char achar (char a, char argc, char ** args, char def)
-{
-	a++;
-	if (a<argc && args[a][0]!='-') return args[a][0];
-	else return def;
-}
-
 char conway (MAT *mat)
 {
 	MAT gen;
-	unsigned l= mat->l;
-	unsigned h= mat->h;
+	int l= mat->l;
+	int h= mat->h;
 	char activity=0;
 	make_r(&gen, l, h);
-	for (unsigned y=0; y<h; y++)
+	for (int y=0; y<h; y++)
 	{
-		for (unsigned x=0; x<l; x++)
+		for (int x=0; x<l; x++)
 		{
 			char n= locals(mat, x, y);
 			if (mat->rack[x][y] && n==2)
@@ -148,15 +167,14 @@ char conway (MAT *mat)
 	return activity;
 }
 
-void get_cells (MAT *mat)
+void input_mode (MAT *mat)
 {
-	unsigned c=0, nx, ny;
-	unsigned l= mat->l;
-	unsigned h= mat->h;
-	printf("\e[1;1H\e[2J");
-	printf("Input cells mode, input X and Y for each cell:\n");
-	printf(" at any time: -1: Show status, -2: Stop input.\n\n");
-	while (!0)
+	int c=0, nx, ny;
+	int l= mat->l;
+	int h= mat->h;
+	printf("Input cells mode, input X and Y for each cell:\n"
+	    " at any time: -1: Show status, -2: Stop input.\n\n");
+	while (1)
 	{
 		printf("X= ");
 		scanf("%d", &nx);
@@ -165,7 +183,8 @@ void get_cells (MAT *mat)
 			print_r(mat,'@','.',0,0,0);
 			continue;
 		}
-		else if (nx<=-2) break;
+		else if (nx<=-2)
+		    break;
 		printf("Y= ");
 		scanf("%d", &ny);
 		if (ny==-1)
@@ -173,17 +192,20 @@ void get_cells (MAT *mat)
 			print_r(mat,'@','.',0,0,0);
 			continue;
 		}
-		else if (ny<=-2) break;
-		if (nx<l && ny<h) mat->rack[nx][ny]=1;
-		else printf("Grid size: %dx%d, change it with -s option\n",l,h);
+		else if (ny<=-2)
+		    break;
+		if (nx<l && ny<h)
+		    mat->rack[nx][ny]=1;
+		else
+		    printf("Grid size: %dx%d, change it with -s option\n",l,h);
 	}
 }
 
-char locals (MAT *mat, unsigned x, unsigned y)
+char locals (MAT *mat, int x, int y)
 {
 	char ul,um,ur,ml,mr,bl,bm,br;
-	unsigned l= mat->l -1;
-	unsigned h= mat->h -1;
+	int l= mat->l -1;
+	int h= mat->h -1;
 	if (y<=0 || x<=0) ul=0;
 	else ul= mat->rack[x-1][y-1];
 	if (y<=0) um=0;
@@ -203,17 +225,17 @@ char locals (MAT *mat, unsigned x, unsigned y)
 	return ul+um+ur+ml+mr+bl+bm+br;
 }
 
-void make_r (MAT *mat, unsigned l, unsigned h)
+void make_r (MAT *mat, int l, int h)
 {
 	mat->rack= malloc(l * sizeof(char*));
 	if (mat->rack == NULL)
 	    exit(1);
-	for (unsigned x=0; x<l; x++)
+	for (int x=0; x<l; x++)
 	{
 		mat->rack[x]= malloc(h * sizeof(char*));
 		if (mat->rack[x] == NULL)
 		    exit(1);
-		for (unsigned y=0; y<h; y++)
+		for (int y=0; y<h; y++)
 		    mat->rack[x][y]= 0;
 	}
 	mat->l= l;
@@ -222,12 +244,12 @@ void make_r (MAT *mat, unsigned l, unsigned h)
 
 void fill_r (MAT *mat, char prob)
 {
-	unsigned l= mat->l;
-	unsigned h= mat->h;
+	int l= mat->l;
+	int h= mat->h;
 	srand(time(NULL));
-	for (unsigned x=0; x<l; x++)
+	for (int x=0; x<l; x++)
 	{
-		for (unsigned y=0; y<h; y++)
+		for (int y=0; y<h; y++)
 		{
 			char random= rand()%100;
 			if (random<prob)
@@ -238,20 +260,20 @@ void fill_r (MAT *mat, char prob)
 
 void print_r (MAT *mat, char a, char d, char delim_z, char delim_v, char delim_c)
 {
-    unsigned l= mat->l;
-	unsigned h= mat->h;
+    int l= mat->l;
+	int h= mat->h;
     if (delim_c)
 	    printf("%c",delim_c);
 	if (delim_z)
-	    for (unsigned i=0; i<=l*2; i++)
+	    for (int i=0; i<=l*2; i++)
 	        printf("%c",delim_z);
 	if (delim_c)
 	    printf("%c\n",delim_c);
-	for (unsigned y=0; y<h; y++)
+	for (int y=0; y<h; y++)
 	{
 		if (delim_v)
 		    printf("%c ",delim_v);
-		for (unsigned x=0; x<l; x++)
+		for (int x=0; x<l; x++)
 		{
 			if (mat->rack[x][y])
 			    printf("%c",a);
@@ -266,7 +288,7 @@ void print_r (MAT *mat, char a, char d, char delim_z, char delim_v, char delim_c
 	if (delim_c)
 	    printf("%c",delim_c);
 	if (delim_z)
-	    for (unsigned i=0; i<=l*2; i++)
+	    for (int i=0; i<=l*2; i++)
 	        printf("%c",delim_z);
 	if (delim_c)
 	    printf("%c\n",delim_c);
