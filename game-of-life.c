22bf6fad253b5db480a9324796a5c190715f7ab2
mb <m.bautista@alumnos.upm.es>
2017-12-07 17:58:43 +0100

    Changed tabs to spaces

--- game-of-life.c
+++ game-of-life.c
@@ -39,28 +39,28 @@ int main (char argc, char **argv)
         " -c C       Represent corner delimiters with C\n"
         " -q         Do not show final message\n"
         " -h         Show this message and exits\n";
-	char txtReset[20];
+    char txtReset[20];
     FILE *fp = popen ("tput reset", "r");
     int szReset= fread (txtReset, 1, sizeof(txtReset), fp);
     pclose (fp);
-	
-	MAT mat;
-	char verbosity= 1;
-	int length= 20;
-	int heigth= 20;
-	int time= 200;
+    
+    MAT mat;
+    char verbosity= 1;
+    int length= 20;
+    int heigth= 20;
+    int time= 200;
     int iter= 0;
     int maxIter= 0; //0 equals infinite
     char appProb=33;
-	char inputMode= 0;
-	char showAllDel= 0;
-	char cAlive='@', cDead=' ';
-	char delHoriz=0, delVert=0, delCorner=0;
-	
-	const char *opts= "hqplx:y:t:i:r:a:d:z:v:c:";
-	int opt;
-	while ((opt= getopt(argc, argv, opts)) != -1)
-	{
+    char inputMode= 0;
+    char showAllDel= 0;
+    char cAlive='@', cDead=' ';
+    char delHoriz=0, delVert=0, delCorner=0;
+    
+    const char *opts= "hqplx:y:t:i:r:a:d:z:v:c:";
+    int opt;
+    while ((opt= getopt(argc, argv, opts)) != -1)
+    {
         switch (opt) {
             case 'h':
                 printf("Usage: %s %s\n", argv[0], txtHelp);
@@ -110,31 +110,31 @@ int main (char argc, char **argv)
                 return 1;
         }
     }
-	
-	make_r(&mat, length, heigth);
-	if (inputMode)
-	{
-	    write(1, txtReset, szReset);
-	    input_mode(&mat);
-	}
-	else
-	    fill_r(&mat, appProb);
-	    
-	if (showAllDel)
-	{
-	    if (!delHoriz) delHoriz= '~';
-	    if (!delVert) delVert= '|';
-	    if (!delCorner) delCorner= '+';
-	}
-	    
-	do //main loop
-	{
-	    write(1, txtReset, szReset);
-		print_r(&mat, cAlive, cDead, delHoriz, delVert, delCorner);
-		usleep(time *1000);
-		iter++;
-	} while (conway(&mat) && iter!=maxIter);
-	
+    
+    make_r(&mat, length, heigth);
+    if (inputMode)
+    {
+        write(1, txtReset, szReset);
+        input_mode(&mat);
+    }
+    else
+        fill_r(&mat, appProb);
+        
+    if (showAllDel)
+    {
+        if (!delHoriz) delHoriz= '~';
+        if (!delVert) delVert= '|';
+        if (!delCorner) delCorner= '+';
+    }
+        
+    do //main loop
+    {
+        write(1, txtReset, szReset);
+        print_r(&mat, cAlive, cDead, delHoriz, delVert, delCorner);
+        usleep(time *1000);
+        iter++;
+    } while (conway(&mat) && iter!=maxIter);
+    
     if (verbosity)
         printf("%d iterations realised.\n", iter);
     free(mat.rack);
@@ -143,153 +143,153 @@ int main (char argc, char **argv)
 
 char conway (MAT *mat)
 {
-	MAT gen;
-	int l= mat->l;
-	int h= mat->h;
-	char activity=0;
-	make_r(&gen, l, h);
-	for (int y=0; y<h; y++)
-	{
-		for (int x=0; x<l; x++)
-		{
-			char n= locals(mat, x, y);
-			if (mat->rack[x][y] && n==2)
-			    gen.rack[x][y]= 1;
-			else if (n==3)
-			    gen.rack[x][y]= 1;
-			else
-			    gen.rack[x][y]= 0;
-			if (mat->rack[x][y] != gen.rack[x][y])
-			    activity=1;
-		}
-	}
-	*mat= gen;
-	return activity;
+    MAT gen;
+    int l= mat->l;
+    int h= mat->h;
+    char activity=0;
+    make_r(&gen, l, h);
+    for (int y=0; y<h; y++)
+    {
+        for (int x=0; x<l; x++)
+        {
+            char n= locals(mat, x, y);
+            if (mat->rack[x][y] && n==2)
+                gen.rack[x][y]= 1;
+            else if (n==3)
+                gen.rack[x][y]= 1;
+            else
+                gen.rack[x][y]= 0;
+            if (mat->rack[x][y] != gen.rack[x][y])
+                activity=1;
+        }
+    }
+    *mat= gen;
+    return activity;
 }
 
 void input_mode (MAT *mat)
 {
-	int c=0, nx, ny;
-	int l= mat->l;
-	int h= mat->h;
-	printf("Input cells mode, input X and Y for each cell:\n"
-	    " at any time: -1: Show status, -2: Stop input.\n\n");
-	while (1)
-	{
-		printf("X= ");
-		scanf("%d", &nx);
-		if (nx==-1)
-		{
-			print_r(mat,'@','.',0,0,0);
-			continue;
-		}
-		else if (nx<=-2)
-		    break;
-		printf("Y= ");
-		scanf("%d", &ny);
-		if (ny==-1)
-		{
-			print_r(mat,'@','.',0,0,0);
-			continue;
-		}
-		else if (ny<=-2)
-		    break;
-		if (nx<l && ny<h)
-		    mat->rack[nx][ny]=1;
-		else
-		    printf("Grid size: %dx%d, change it with -s option\n",l,h);
-	}
+    int c=0, nx, ny;
+    int l= mat->l;
+    int h= mat->h;
+    printf("Input cells mode, input X and Y for each cell:\n"
+        " at any time: -1: Show status, -2: Stop input.\n\n");
+    while (1)
+    {
+        printf("X= ");
+        scanf("%d", &nx);
+        if (nx==-1)
+        {
+            print_r(mat,'@','.',0,0,0);
+            continue;
+        }
+        else if (nx<=-2)
+            break;
+        printf("Y= ");
+        scanf("%d", &ny);
+        if (ny==-1)
+        {
+            print_r(mat,'@','.',0,0,0);
+            continue;
+        }
+        else if (ny<=-2)
+            break;
+        if (nx<l && ny<h)
+            mat->rack[nx][ny]=1;
+        else
+            printf("Grid size: %dx%d, change it with -s option\n",l,h);
+    }
 }
 
 char locals (MAT *mat, int x, int y)
 {
-	char ul,um,ur,ml,mr,bl,bm,br;
-	int l= mat->l -1;
-	int h= mat->h -1;
-	if (y<=0 || x<=0) ul=0;
-	else ul= mat->rack[x-1][y-1];
-	if (y<=0) um=0;
-	else um= mat->rack[x-0][y-1];
-	if (y<=0 || x>=l) ur=0;
-	else ur= mat->rack[x+1][y-1];
-	if (x<=0) ml=0;
-	else ml= mat->rack[x-1][y-0];
-	if (x>=l) mr=0;
-	else mr= mat->rack[x+1][y-0];
-	if (y>=h || x<=0) bl=0;
-	else bl= mat->rack[x-1][y+1];
-	if (y>=h) bm=0;
-	else bm= mat->rack[x-0][y+1];
-	if (y>=h || x>=l) br=0;
-	else br= mat->rack[x+1][y+1];
-	return ul+um+ur+ml+mr+bl+bm+br;
+    char ul,um,ur,ml,mr,bl,bm,br;
+    int l= mat->l -1;
+    int h= mat->h -1;
+    if (y<=0 || x<=0) ul=0;
+    else ul= mat->rack[x-1][y-1];
+    if (y<=0) um=0;
+    else um= mat->rack[x-0][y-1];
+    if (y<=0 || x>=l) ur=0;
+    else ur= mat->rack[x+1][y-1];
+    if (x<=0) ml=0;
+    else ml= mat->rack[x-1][y-0];
+    if (x>=l) mr=0;
+    else mr= mat->rack[x+1][y-0];
+    if (y>=h || x<=0) bl=0;
+    else bl= mat->rack[x-1][y+1];
+    if (y>=h) bm=0;
+    else bm= mat->rack[x-0][y+1];
+    if (y>=h || x>=l) br=0;
+    else br= mat->rack[x+1][y+1];
+    return ul+um+ur+ml+mr+bl+bm+br;
 }
 
 void make_r (MAT *mat, int l, int h)
 {
-	mat->rack= malloc(l * sizeof(char*));
-	if (mat->rack == NULL)
-	    exit(1);
-	for (int x=0; x<l; x++)
-	{
-		mat->rack[x]= malloc(h * sizeof(char*));
-		if (mat->rack[x] == NULL)
-		    exit(1);
-		for (int y=0; y<h; y++)
-		    mat->rack[x][y]= 0;
-	}
-	mat->l= l;
-	mat->h= h;
+    mat->rack= malloc(l * sizeof(char*));
+    if (mat->rack == NULL)
+        exit(1);
+    for (int x=0; x<l; x++)
+    {
+        mat->rack[x]= malloc(h * sizeof(char*));
+        if (mat->rack[x] == NULL)
+            exit(1);
+        for (int y=0; y<h; y++)
+            mat->rack[x][y]= 0;
+    }
+    mat->l= l;
+    mat->h= h;
 }
 
 void fill_r (MAT *mat, char prob)
 {
-	int l= mat->l;
-	int h= mat->h;
-	srand(time(NULL));
-	for (int x=0; x<l; x++)
-	{
-		for (int y=0; y<h; y++)
-		{
-			char random= rand()%100;
-			if (random<prob)
-			    mat->rack[x][y]=1;
-		}
-	}
+    int l= mat->l;
+    int h= mat->h;
+    srand(time(NULL));
+    for (int x=0; x<l; x++)
+    {
+        for (int y=0; y<h; y++)
+        {
+            char random= rand()%100;
+            if (random<prob)
+                mat->rack[x][y]=1;
+        }
+    }
 }
 
 void print_r (MAT *mat, char a, char d, char delim_z, char delim_v, char delim_c)
 {
     int l= mat->l;
-	int h= mat->h;
+    int h= mat->h;
     if (delim_c)
-	    printf("%c",delim_c);
-	if (delim_z)
-	    for (int i=0; i<=l*2; i++)
-	        printf("%c",delim_z);
-	if (delim_c)
-	    printf("%c\n",delim_c);
-	for (int y=0; y<h; y++)
-	{
-		if (delim_v)
-		    printf("%c ",delim_v);
-		for (int x=0; x<l; x++)
-		{
-			if (mat->rack[x][y])
-			    printf("%c",a);
-			else
-			    printf("%c",d);
-			printf(" ");
-		}
-		if (delim_v)
-		    printf("%c",delim_v);
-		printf("\n");
-	}
-	if (delim_c)
-	    printf("%c",delim_c);
-	if (delim_z)
-	    for (int i=0; i<=l*2; i++)
-	        printf("%c",delim_z);
-	if (delim_c)
-	    printf("%c\n",delim_c);
+        printf("%c",delim_c);
+    if (delim_z)
+        for (int i=0; i<=l*2; i++)
+            printf("%c",delim_z);
+    if (delim_c)
+        printf("%c\n",delim_c);
+    for (int y=0; y<h; y++)
+    {
+        if (delim_v)
+            printf("%c ",delim_v);
+        for (int x=0; x<l; x++)
+        {
+            if (mat->rack[x][y])
+                printf("%c",a);
+            else
+                printf("%c",d);
+            printf(" ");
+        }
+        if (delim_v)
+            printf("%c",delim_v);
+        printf("\n");
+    }
+    if (delim_c)
+        printf("%c",delim_c);
+    if (delim_z)
+        for (int i=0; i<=l*2; i++)
+            printf("%c",delim_z);
+    if (delim_c)
+        printf("%c\n",delim_c);
 }
